ec9fc462e0bbb92f3ddfcfe7ff41bf373c1be663
Kevinjose102 <kevinjose2505@gmail.com>
2025-03-11 22:37:12 +0530

    updated about.js

--- src/about.js
+++ src/about.js
@@ -1,6 +1,21 @@
 const about = function(){
+    const about = document.createElement("div");
+    about.classList.add("about-content")
+
+    const chefImage = document.createElement("img");
+    chefImage.src = require("./burger-chef.jpg")
+    chefImage.classList.add("chef-image")
+
+    const quote = document.createElement("div")
+    quote.classList.add("quote")
+    quote.textContent = "\"Twobuns was founded with the simple goal of providing the best burgers in town. What started as a passion for cooking quickly grew into a beloved neighborhood spot known for its quality ingredients and mouth-watering flavors\""
+    
+    about.appendChild(chefImage)
+    about.appendChild(quote)
+    
     const content = document.getElementById("content");
     content.innerHTML = '';
+    content.appendChild(about)
 }
 
 export default about;
